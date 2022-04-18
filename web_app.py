f430b606bd835abb760e6d1cb1ee9a151d70fc10
AlabamaBroadcastMediaPage <101920870+AlabamaBroadcastMediaPage@users.noreply.github.com>
2022-04-18 16:49:56 +0200

    Streamlit webapp uploaded

---
+++ web_app.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plot
+import numpy as np
+import streamlit as st
+
+st.write("""
+# News Article Classifier App
+
+### Classify your news articles with different machine learning models!
+""")
+
