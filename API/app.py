267d3f1c8424da9f0abe2d49a11b4646d872930c
PeterPottR <62199244+PeterPottR@users.noreply.github.com>
2022-05-21 09:03:16 +0200

    update

--- API/app.py
+++ API/app.py
@@ -1,13 +1,24 @@
 from urllib import response
 from flask import Flask
 from flask import request
+from sklearn.linear_model import LogisticRegression
+import pickle
 
 app = Flask(__name__)
 
 @app.route("/getclassification", methods=['GET'])
 def serve_foo():
     
+    pos = request.json['q']['pos']
+    neg = request.json['q']['neg']
+    author = request.json['q']['author']
+    site = request.json['q']['site']
+
+
+    loaded_model = pickle.load(open("model.pkl", 'rb'))
+
     
+
     accuracy = 0
     positive = True
     if(positive):
--- web_app.py
+++ web_app.py
@@ -3,8 +3,23 @@ import pandas as pd
 import streamlit as st
 import requests
 from flask import Flask
+from io import StringIO
+import nltk
 
+nltk.download('punkt')
 
+def articleSentenceClassification(content):
+    pos_words = ['jó','gyönyörű','szép','pozitív','csodálatos','helyes','elfogadható','jobb','szebb']
+    x=0
+    y=0
+    s_list = nltk.tokenize.sent_tokenize(content)
+    for i in range(len(s_list)):
+        for word in pos_words:
+            if (s_list[i].find(word) != -1):
+                x+=1
+    y = len(s_list)-x
+    st.write(x,y)
+    return x,y
 
 st.write("""
 # News Article Classifier App
@@ -14,32 +29,25 @@ st.write("""
 
 st.write("Who is the author of the article?")
 author = st.text_input('Author name')
-st.write("How many positive sentences does this article have?")
-positive_num = st.number_input('Positive sentences', step=1, min_value=0)
-st.write("How many negative sentences does this article have?")
-negative_num = st.number_input('Negative sentences', step=1, min_value=0)
 
+site = st.selectbox('What site hosted this article?',('Mandiner', 'Krónika Online', 'HVG'))
 
-model = st.radio(
-    "What machine learning model do you want to use?",
-    ('Logistic Regression', 'Random Forest Classification'))
+uploaded_file = st.file_uploader("Choose a file where you upload the article")
+if uploaded_file is not None:
+    article_content = StringIO(uploaded_file.getvalue().decode("utf-8")).read()
+    pos_count,neg_count = articleSentenceClassification(article_content)
 
 if st.button('Classify'):
-
-    if(model =='Logistic Regression'):
-        model_num=1
-    else:
-        model_num=2
-
     url = "http://peterpottr.pythonanywhere.com/getclassification"
+    #url = "http://127.0.0.1:5000/getclassification"
     my_headers = {
     'Authorization': 'XXXXX',}
     data = { 
     "q": {
         "author": author,
-        "pos": positive_num,
-        "neg": negative_num,
-        "model": model_num 
+        "site": site,
+        "pos": pos_count,
+        "neg": neg_count
     }}
     r = requests.get(url, headers=my_headers, json=data)
-    st.write(r.content)
+    st.write(r.text)
